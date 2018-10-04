4c127e9320e09ffe6f89df8d4bac5bb213ec32c6
Burak Olgun <burakolgun91@gmail.com>
2018-10-04 09:36:15 +0300

    Add test login user

--- controller/authentication.go
+++ controller/authentication.go
@@ -1,17 +1,44 @@
 package controller
 
 import (
+	"../models"
+	services "../service/auth_service"
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
 )
 
 func Register(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello, World from register"))
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("%v", body)
+	w.Write(body)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+	requestUser := new(models.User)
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&requestUser)
+	responseStatus, token := services.Login(requestUser)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(responseStatus)
+	w.Write(token)
+}
+
+func RefreshToken(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	requestUser := new(models.User)
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&requestUser)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(services.RefreshToken(requestUser))
+}
+
+func Logout(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	err := services.Logout(r)
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
 }
---
+++ service/auth_service/auth.go
@@ -0,0 +1,50 @@
+package auth_service
+
+import (
+	"../../api/parameters"
+	"../../models"
+	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
+	"net/http"
+)
+
+func Login(requestUser *models.User) (int, []byte) {
+	authBackend := InitJWTAuthenticationBackend()
+	if authBackend.Authenticate(requestUser) {
+		token, err := authBackend.GenerateToken(requestUser.UUID)
+		if err != nil {
+			return http.StatusInternalServerError, []byte("")
+		} else {
+			response, _ := json.Marshal(parameters.TokenAuthentication{token})
+			return http.StatusOK, response
+		}
+	}
+
+	return http.StatusUnauthorized, []byte("")
+}
+
+func RefreshToken(requestUser *models.User) []byte {
+	authBackend := InitJWTAuthenticationBackend()
+	token, err := authBackend.GenerateToken(requestUser.UUID)
+	if err != nil {
+		panic(err)
+	}
+	response, err := json.Marshal(parameters.TokenAuthentication{token})
+	if err != nil {
+		panic(err)
+	}
+	return response
+}
+
+func Logout(req *http.Request) error {
+	authBackend := InitJWTAuthenticationBackend()
+	tokenRequest, err := request.ParseFromRequest(req, request.OAuth2Extractor, func(token *jwt.Token) (interface{}, error) {
+		return authBackend.PublicKey, nil
+	})
+	if err != nil {
+		return err
+	}
+	tokenString := req.Header.Get("Authorization")
+	return authBackend.Logout(tokenString, tokenRequest)
+}
